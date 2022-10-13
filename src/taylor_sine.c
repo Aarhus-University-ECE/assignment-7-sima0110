2efa94c56bd135c824ca25a83f2972962304c50b
sima0110 <114432467+sima0110@users.noreply.github.com>
2022-10-13 19:14:45 +0200

    prog

--- src/taylor_sine.c
+++ src/taylor_sine.c
@@ -1,7 +1,18 @@
 #include "taylor_sine.h"
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+
+
+double factorial(int x){ //function that calculates the factorial 
+    if (x==0) 
+        return 1;
+    else
+    return(x * factorial(x - 1)); 
+
 double taylor_sine(double x, int n)
 {
     /* implement your function here */
-    
+    assert(n > 0); //checks the precondition//
     return -1.0;
 }
